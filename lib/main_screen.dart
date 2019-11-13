fa62bf6863ee47028001a6f4f67e89d883e140d1
Fanni Nagy <nfanni97@gmail.com>
2019-11-13 21:19:09 +0100

    added fancy font

---
+++ lib/main_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Boldog karácsonyt!',
+          style: Theme.of(context).textTheme.title,
+        ),
+      ),
+    );
+  }
+}
