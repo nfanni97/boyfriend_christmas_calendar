d3e4b2df680fb9051208cd7ddf0952837cb63187
Fanni Nagy <nfanni97@gmail.com>
2019-11-14 17:44:29 +0100

    base surprise screen

---
+++ lib/screens/base_surprise_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BaseSurpriseScreen extends StatelessWidget {
+  final int id;
+  final int day;
+  final Widget content;
+
+  BaseSurpriseScreen({@required this.id,@required this.day,@required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //TODO: snowy arrow as leading element?
+      appBar: AppBar(
+        title: Text(
+          'Dec. $day',
+          style: Theme.of(context).textTheme.title,
+        ),
+      ),
+      body: content,
+    );
+  }
+}
