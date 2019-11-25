5c4f2708635a003cbd9e079e012d72d19076942b
Fanni Nagy <nfanni97@gmail.com>
2019-11-25 14:36:48 +0100

    day 3

--- lib/widgets/days/day_3.dart
+++ lib/widgets/days/day_3.dart
@@ -2,8 +2,23 @@ import 'package:flutter/material.dart';
 
 class Day3 extends StatelessWidget {
   const Day3();
+  static const intro = '''
+    Movie night!
+    Annyit meséltél róla, biztos jó :)
+    ''';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Text(
+          intro,
+          textAlign: TextAlign.center,
+        ),
+        Image.asset(
+          'assets/imgs/day_03.jpg',
+          height: MediaQuery.of(context).size.height * 0.7,
+        ),
+      ],
+    );
   }
 }
