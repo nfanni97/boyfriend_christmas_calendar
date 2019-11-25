bc9358ce7daf647f2bcfec07a4a51d823b6647b1
Fanni Nagy <nfanni97@gmail.com>
2019-11-25 11:03:39 +0100

    day 02

--- lib/widgets/days/day_2.dart
+++ lib/widgets/days/day_2.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 
 class Day2 extends StatelessWidget {
   const Day2();
+  static const intro = '''
+      Csak hogy nehogy elfelejtsd hogy voltunk ám Koperben ;)
+      P.S.: gyere átvenni az ajándékod (:
+      ''';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        //TODO: size according to size of bells - adjust size of image too!
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          intro,
+          textAlign: TextAlign.center,
+        ),
+        Image.asset(
+          'assets/imgs/day_02.jpg',
+          height: MediaQuery.of(context).size.height * 0.7,
+        ),
+      ],
+    );
   }
 }
