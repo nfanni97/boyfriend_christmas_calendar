1d76cb0e8794dc9c396d1ef49bf9faa99587a5b7
Fanni Nagy <nfanni97@gmail.com>
2019-11-26 16:14:13 +0100

    day 10

--- lib/widgets/days/day_10.dart
+++ lib/widgets/days/day_10.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 
 class Day10 extends StatelessWidget {
   const Day10();
+  static const text = '''Már annyi medvét kaptam tőled, és még egyet se adtam.
+De ez már nem sokáig marad így...
+Gyere átvenni a mai apróságot!''';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.body1.copyWith(fontSize: 40),
+      ),
+    );
   }
 }
