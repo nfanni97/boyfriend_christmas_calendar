fda047646117409a12b0ad3d04365c5de9231298
Fanni Nagy <nfanni97@gmail.com>
2019-11-27 14:52:21 +0100

    day 14

--- lib/widgets/days/day_14.dart
+++ lib/widgets/days/day_14.dart
@@ -2,8 +2,25 @@ import 'package:flutter/material.dart';
 
 class Day14 extends StatelessWidget {
   const Day14();
+  static const intro = '''(A mai meglepi átvihető másik napra is.)
+Menjünk korizni!
+Azt nem mondom, hogy ilyeneket fogunk csinálni, de azért biztos jó lesz :D''';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text(
+            intro,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Image.asset(
+          'assets/imgs/day_14.jpg',
+          height: MediaQuery.of(context).size.height * 0.5,
+        ),
+      ],
+    );
   }
 }
