e7e4fee67ae3fee60233d1b640741075d7b1c433
Fanni Nagy <nfanni97@gmail.com>
2019-12-01 00:23:48 +0100

    day 15

--- lib/widgets/days/day_15.dart
+++ lib/widgets/days/day_15.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 
 class Day15 extends StatelessWidget {
   const Day15();
+  static const intro =
+      'Azt nem mondom, hogy ilyen szépeket fogunk csinálni, de ígérem nagyon finomat kapsz :P';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Text(
+          intro,
+          textAlign: TextAlign.center,
+        ),
+        Image.asset(
+          'assets/imgs/day_15.jpg',
+          height: MediaQuery.of(context).size.height * 0.5,
+        ),
+      ],
+    );
   }
 }
