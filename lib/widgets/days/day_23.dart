95499f7c68f3ff53d424f6aeff379c06554c7222
Fanni Nagy <nfanni97@gmail.com>
2019-12-01 00:59:53 +0100

    day 23

--- lib/widgets/days/day_23.dart
+++ lib/widgets/days/day_23.dart
@@ -2,8 +2,24 @@ import 'package:flutter/material.dart';
 
 class Day23 extends StatelessWidget {
   const Day23();
+  static const intro =
+      'A kedvenc személyemnek. Nézegesd a kedvenc szemeimmel :P (na jó ez kicsit creepy)';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text(
+            intro,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Image.asset(
+          'assets/imgs/day_23.jpg',
+          height: MediaQuery.of(context).size.height * 0.5,
+        ),
+      ],
+    );
   }
 }
