a55ac7e02fe56749a41f651c152ee1ef06c9d1f7
Fanni Nagy <nfanni97@gmail.com>
2019-12-01 00:20:16 +0100

    switched day 15 and 18

--- lib/widgets/days/day_15.dart
+++ lib/widgets/days/day_15.dart
@@ -2,28 +2,8 @@ import 'package:flutter/material.dart';
 
 class Day15 extends StatelessWidget {
   const Day15();
-  static const intro =
-      '''Ugyan már rég elmúlt Mikulás, viszont közeledik a karácsony. Néhol pedig a karácsonyi ajándékokat a Télapó hozza, úgyhogy most azt kapsz egyet, hogy legyen az angyalnak egy kis segítsége :)
-P.S.: este gyere átvenni a meglepit ;)''';
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 8.0,
-            horizontal: 35.0,
-          ),
-          child: Text(
-            intro,
-            textAlign: TextAlign.center,
-          ),
-        ),
-        Image.asset(
-          'assets/imgs/day_15.jpg',
-          height: MediaQuery.of(context).size.height * 0.43,
-        ),
-      ],
-    );
+    return Container();
   }
 }
